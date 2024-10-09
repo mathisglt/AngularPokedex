59bea32a4c233bb30cf270a38d9a163a39316785
Mathis <mathis.gaultier@etudiant.univ-rennes1.fr>
2024-10-09 08:10:35 +0200

    fin j1

---
+++ src/app/pokemon.ts
@@ -0,0 +1,8 @@
+export class Pokemon {
+    nom:string;
+    id:string;
+    constructor(nom ="Default",id="IDDefault") {
+        this.nom = nom;
+        this.id = id;
+    } 
+}
---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Pokemon } from '../pokemon';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css'
+})
+export class SearchBarComponent {
+  id: string = '';
+  lpoke: Pokemon[] = [
+  new Pokemon("Salamèche","1"),
+  new Pokemon("Tiplouf","2"),
+  new Pokemon("Ouisticram","3"),
+  new Pokemon("Bulbizarre","4"),
+  new Pokemon("Tortipousse","5")
+  ]
+}
