849d98632863b07109a74d9456d047b73da038bb
magaultier <mathis.gaultier@etudiant.univ-rennes1.fr>
2024-11-02 01:27:41 +0100

    dernieres modifs

--- src/app/pokeapi.service.ts
+++ src/app/pokeapi.service.ts
@@ -10,11 +10,11 @@ export class PokeapiService {
 
   constructor(private http: HttpClient) { }
 
-  // Méthode pour récupérer la liste des Pokémon
+
   getPokemons(): Observable<PokeModel>{
-    return this.http.get<PokeModel>("https://pokeapi.co/api/v2/pokemon?limit=1025&offset=0"); // URL de l'API pour récupérer le Pokédex
+    return this.http.get<PokeModel>("https://pokeapi.co/api/v2/pokemon?limit=1025&offset=0"); 
   }
   getPokemonDetails(id:string): Observable<PokeModelDetails>{
-    return this.http.get<PokeModelDetails>("https://pokeapi.co/api/v2/pokemon/"+id); // URL de l'API pour récupérer le Pokédex
+    return this.http.get<PokeModelDetails>("https://pokeapi.co/api/v2/pokemon/"+id); 
   }
 }
